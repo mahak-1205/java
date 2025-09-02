8053569d220b0f81bb8d506f570203d68da0ff2e
mahak-1205 <mehak.rvansh@gmail.com>
2025-09-02 07:59:16 -0700

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,18 @@
+public class loops {
+    public static void main(String[] args) {
+        // for(int a = 1; a < 11; a++){
+        //     System.out.println(a);
+        // }
+        // int i = 0;
+        // while(i < 11){
+        //     System.out.println(i);
+        //     i = i +1;
+        // }
+        int j = 1;
+do{
+    System.out.println(j);
+    j = j + 1;
+} while(j<11);
+    }
+    
+}
